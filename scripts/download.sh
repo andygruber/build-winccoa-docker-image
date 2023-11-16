1881e0a1f32df4f8b242535a8d8ba8efe11bd70d
Andreas Gruber <andreas@gruber.info>
2023-11-17 00:44:15 +0100

    Just get the first found patch-level for Debian WinCC OA 3.19

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,21 +1,37 @@
 #!/bin/bash
 
-# File containing the HTML content
-HTML_FILE="Downloads.html"
-
+# Files containing the HTML content
+HTML1_FILE="login.html"
+HTML2_FILE="all319platforms.html"
+HTML3_FILE="debiandownload.html"
 # Define the base URL
 BASE_URL="https://www.winccoa.com"
 
 OUTPUT_FILE="WinCC_OA.zip"
 
-PATCH_NUM="p007"
+echo "Login to ${BASE_URL}"
+curl -o "$HTML1_FILE" -c cookie.txt -d "user=${USERNAME}&pass=${PASSWORD}&logintype=login" -X POST "${BASE_URL}/index.html?tx_felogin_login%5Baction%5D=login&amp;tx_felogin_login%5Bcontroller%5D=Login"
 
-curl -o "$HTML_FILE" -c cookie.txt -d "user=${USERNAME}&pass=${PASSWORD}&logintype=login" -X POST "${BASE_URL}/index.html?tx_felogin_login%5Baction%5D=login&amp;tx_felogin_login%5Bcontroller%5D=Login"
+echo "Open main Downloadpage"
+curl -o "$HTML2_FILE" -b cookie.txt "${BASE_URL}/downloads/category/wincc-oa-319.html"
 
-curl -o "$HTML_FILE" -b cookie.txt "${BASE_URL}/downloads/detail/wincc-oa-319-${PATCH_NUM}-debian-11-bullseye.html"
+echo "Search for download link"
+LINK=$(awk -v RS="</a>" '
+    /WinCC OA 3.19/ && /Debian 11 \(Bullseye\)/ && !/arm64/ {
+        match($0, /href="([^"]+)"/, arr)
+        if (arr[1] != "") {
+            print arr[1]
+            exit
+        }
+    }
+' $HTML2_FILE | sed 's/\&amp;/\&/g')
+
+echo "Opening link: ${BASE_URL}/${LINK}"
+
+curl -o "$HTML3_FILE" -b cookie.txt "${BASE_URL}/${LINK}"
 
 # Extract the relative URL
-RELATIVE_URL=$(grep "Download ZIP File" $HTML_FILE | grep -v "arm64" | sed -n 's/.*href="\([^"]*\).*/\1/p' | sed 's/\&amp;/\&/g')
+RELATIVE_URL=$(grep "Download ZIP File" $HTML3_FILE | grep -v "arm64" | sed -n 's/.*href="\([^"]*\).*/\1/p' | sed 's/\&amp;/\&/g')
 
 # Create the full URL
 FULL_URL="${BASE_URL}${RELATIVE_URL}"
